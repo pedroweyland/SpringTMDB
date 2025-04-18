4ab0fe6b9e058a66d5c0e01fc00af5a982895f5f
Pedro Weyland <weylandpedro@gmail.com>
2025-04-18 15:21:18 -0300

    Config: Configuro msvc-media
    - Arreglo lombok ya que la version anterior no era compatible
    
    - Agrego FeignClient como dependencia para poder hacer peticiones

--- msvc-media/src/main/java/com/themoviedb/media/MsvcMediaApplication.java
+++ msvc-media/src/main/java/com/themoviedb/media/MsvcMediaApplication.java
@@ -2,10 +2,13 @@ package com.themoviedb.media;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class MsvcMediaApplication {
 
 	public static void main(String[] args) {
