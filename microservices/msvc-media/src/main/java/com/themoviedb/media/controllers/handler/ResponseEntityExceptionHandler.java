393fe32069b26b13c2cc74b00935a70f6d995a5f
Pedro Weyland <weylandpedro@gmail.com>
2025-06-09 12:32:24 -0300

    Refactor: reestructuracion del proyecto
    - Ahora mis modulos 'msvc-config' y 'msvc-eureka' les cambio el nombre por 'config-server' y 'eureka-server' respectivamente
    
    - Ahora creo un modulo 'microservices' que contendra mis microservicios de 'auth' - 'media' - 'gateway', seria un modulo nuevo que es padre de los microservicos mencionados y es hijo del proyecto padre SpringTMDB

---
+++ microservices/msvc-media/src/main/java/com/themoviedb/media/controllers/handler/ResponseEntityExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.themoviedb.media.controllers.handler;
+
+import com.themoviedb.media.exception.MediaNotFoundException;
+import com.themoviedb.media.exception.PageNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class ResponseEntityExceptionHandler extends org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {PageNotFoundException.class, MediaNotFoundException.class})
+    protected ResponseEntity<Object> handleMateriaNotFound(Exception ex, WebRequest request) {
+        String exceptionMessage = ex.getMessage();
+        CustomApiError error = new CustomApiError();
+
+        error.setErrorCode(404);
+        error.setErrorMessage(exceptionMessage);
+
+        return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(value = {IllegalArgumentException.class, RuntimeException.class})
+    protected ResponseEntity<Object> handleBadRequest(Exception ex, WebRequest request) {
+        String exceptionMessage = ex.getMessage();
+        CustomApiError error = new CustomApiError();
+
+        error.setErrorCode(400);
+        error.setErrorMessage(exceptionMessage);
+
+        return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        if (body == null) {
+            CustomApiError error = new CustomApiError();
+            error.setErrorMessage(ex.getMessage());
+            body = error;
+        }
+
+        return new ResponseEntity<>(body, headers, status);
+    }
+}
